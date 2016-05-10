333c9a47ed541e502767b88cd740c6005c309e3e
Kirill B <miaplanedo@mail.ru>
2016-05-10 15:03:42 +0300

    commenting

--- hough.cpp
+++ hough.cpp
@@ -38,6 +38,12 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // required arguments
+    if(!lflag || !iflag){
+        usage();
+        return 0;
+    }
+
     cv::Mat original = cv::imread(filename);
     std::cout << "Original image: " << filename << std::endl;
 
@@ -53,16 +59,15 @@ void houghLine(cv::Mat original, int limit) {
     cv::Mat src, rgb, bin;
     src = original.clone();
 
-    rgb = cv::Mat(src.size(), 8, 3);
+    rgb = cv::Mat(src.size(), IPL_DEPTH_8U, 3);
 
     cv::cvtColor(src, rgb, cv::COLOR_BGR2GRAY);
 
     bin = cv::Mat(src.size(), IPL_DEPTH_8U, 1);
+    // find contour with Canny algorithm
     cv::Canny(src, bin, 50, 200);
 
-    cv::namedWindow( "bin", cv::WINDOW_NORMAL);
-    cv::imshow( "bin", bin );
-
+    // max distance from coords origin
     int RMax = cvRound( sqrt( (double)(src.cols * src.cols + src.rows * src.rows) ) );
     cv::Mat phase = create_phase(src, bin, RMax);
 
@@ -71,6 +76,7 @@ void houghLine(cv::Mat original, int limit) {
     
     for(int f = 0; f < 180; f++){
         short* ptrP = (short*) (phase.data + f * phase.step);
+        // by all distancies
         for(int r = 0; r < RMax; r++) {
             if(ptrP[r] >= limit) {
                 thetas.push_back(f);
@@ -79,6 +85,7 @@ void houghLine(cv::Mat original, int limit) {
         }
     }
     
+    // draw all found lines
     for(int y = 0; y < rgb.rows; y++) {
         uchar* ptr = (uchar*) (rgb.data + y * rgb.step);
         for(int x = 0; x < rgb.cols; x++){
@@ -88,15 +95,18 @@ void houghLine(cv::Mat original, int limit) {
 
                 if ( cvRound(((y) * sin(Theta) + (x) * cos(Theta))) == rs[i]) {
                     ptr[x] = 0;
-                    ptr[x+1] = 0;
-                    ptr[x+2] = 255;
+                    ptr[x + 1] = 255;
+                    ptr[x + 2] = 0;
                 }
             }
         }
     }
 
-    cv::namedWindow( "line", cv::WINDOW_NORMAL);
-    cv::imshow( "line", rgb );
+    cv::namedWindow( "CONTOUR", cv::WINDOW_NORMAL);
+    cv::imshow( "CONTOUR", bin );
+    
+    cv::namedWindow( "FOUND LINES", cv::WINDOW_NORMAL);
+    cv::imshow( "FOUND LINES", rgb );
     cv::waitKey(0);
 }
 
@@ -105,17 +115,22 @@ cv::Mat create_phase(cv::Mat src, cv::Mat bin, int RMax){
     
     int x = 0, y = 0, r = 0, f = 0;
     cv::Mat phase = cv::Mat(cv::Size(RMax, 180), IPL_DEPTH_16U, 1);
-   
+
     for(y = 0; y < bin.rows; y++){
         uchar* ptr = (uchar*) (bin.data + y * bin.step);
         for(x = 0; x < bin.cols; x++){
+            // is it contour?
             if(ptr[x] > 0) {
                 for(int f = 0; f < 180; f++) {
                     short* ptrP = (short*) (phase.data + f * phase.step);
+                    // by all distancies
                     for(int r = 0; r < RMax; r++) {
+                        // grads to rads
                         float theta = f * CV_PI / 180.0; 
 
+                        // math is exact but life is full of accidents
                         if ( abs(( (y)*sin(theta) + (x)*cos(theta)) - r) < ACCURANCY ) {
+                            // point in Hough space
                             ptrP[r]++;
                         }
                     }
